e51cf4a0d8d117f19347238a19f11bf107834a8e
Max <max@mgrev.com>
2019-02-26 09:52:30 -0600

    Initial Commit

---
+++ getToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getToDo
+//
+//  Created by Max Grev on 2/26/19.
+//  Copyright © 2019 Upstate.Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
