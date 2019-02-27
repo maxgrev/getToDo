d5c1baf94f45ec0a761f2c7b4fc0f3c6cbf77bc0
Max <max@mgrev.com>
2019-02-27 10:01:46 -0600

    ToDo list complete with datasource and delegate methods

---
+++ getToDo/getToDoViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  getToDo
+//
+//  Created by Max Grev on 2/26/19.
+//  Copyright © 2019 Upstate.Media. All rights reserved.
+//
+
+import UIKit
+
+class getToDoViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //Declare how many rows (numberOfRowsInSection) do we want in our TableView
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "getToDoItemCell")
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
+
